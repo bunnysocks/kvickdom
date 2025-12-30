502304a4ff46afd1f39cbf2b2bcc88e7668ce02b
bunnysocks <hemansakkthivelms14@gmail.com>
2025-12-30 15:05:38 +0530

    mounted vNode with target

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import createElement from "./vdomm/createElement";
+import createElement from "./vdomm/createElement.js";
+import render from "./vdomm/render.js";
+import mount from "./vdomm/mount.js";
 
 const vApp = createElement(
     'div',
     {
         attrs: {id : 'app'},
-        children: [] 
+        children: [
+            createElement(
+                'img',
+                {
+                    attrs: {
+                     src : "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bHNreDRjZWNxZ2liaTA4bm4xYzVmejNvdzNiNWdiNzM3aGRiZDhsayZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/MDJ9IbxxvDUQM/giphy.gif"
+                    }
+                }
+            )
+        ] 
     }
 )
-console.log(vApp)
+
+const $app = render(vApp)
+mount($app, document.getElementById("app"))
+console.log($app)
---
+++ src/vdomm/render.js
@@ -0,0 +1,16 @@
+export default render = (vNode) => {
+    const $el = document.createElement(vNode.tagName);
+
+    //set attributes
+    for(const [k,v] of Object.entries(vNode.attrs)) {
+        $el.setAttribute(k,v)
+    }
+
+    //set children
+    for(const child of vNode.children) {
+        const $child = render(child)
+        $el.appendChild($child)
+    }
+
+    return $el
+}
